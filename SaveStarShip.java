cdbae647cba8a0fdc4366229db9fdcf96e0d7e32
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-08-21 21:58:43 +0200

    added method SaveStarShip v.1

--- SaveStarShip.java
+++ SaveStarShip.java
@@ -1,2 +1,14 @@
-package main;public class SaveStarShip {
+package main;
+
+public class SaveStarShip {
+    public int calculateDistance(int distance){
+        return Math.abs(distance);
+    }
+    //Test output
+    public static void main(String[] args) {
+        SaveStarShip ship = new SaveStarShip();
+
+        //Should be 10
+        System.out.println(ship.calculateDistance(-10));
+    }
 }
