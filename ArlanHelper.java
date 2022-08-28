e1a4339b03ef547533f6d0f22f64c46e28897d27
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-08-29 00:14:09 +0200

    added method drawRect

--- ArlanHelper.java
+++ ArlanHelper.java
@@ -1,6 +1,22 @@
 package main;
 
 public class ArlanHelper {
+    public String drawRect(int width, int height, char c){
+        int row = 1;
+        int line = 1;
+        String result = "";
+        while (line <= height) {
+            while (row <= width) {
+                result += c;
+                row++;
+            }
+            result+="\n";
+            row = 1;
+            line++;
+        }
+        return result;
+    }
+
     public String drawQuad(int i) {
         int row = 1;
         int line = 1;
@@ -23,7 +39,9 @@ public class ArlanHelper {
         //Should be:
         //**
         //**
-        System.out.println(helper.drawQuad(5));
+        System.out.println(helper.drawQuad(2));
+
+        System.out.println(helper.drawRect(5, 5, 'X'));
     }
 
 }
