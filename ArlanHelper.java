9c1cf893ba51c7560e983967254de245937eedc6
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-08-28 23:50:52 +0200

    added method drawQuad

--- ArlanHelper.java
+++ ArlanHelper.java
@@ -1,4 +1,30 @@
 package main;
 
 public class ArlanHelper {
+    public String drawQuad(int i) {
+        int row = 1;
+        int line = 1;
+        String result = "";
+        while (line <= i) {
+            while (row <= i) {
+                result += "*";
+                row++;
+            }
+            result+="\n";
+            row = 1;
+            line++;
+        }
+
+        return result;
+    }
+
+    //Test output
+    public static void main(String[] args) {
+        ArlanHelper helper = new ArlanHelper();
+        //Should be:
+        //**
+        //**
+        System.out.println(helper.drawQuad(5));
+    }
+
 }
