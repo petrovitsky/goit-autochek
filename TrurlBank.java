1e3ada97ae2e61192cd8fe1aabe34151ddb34b2a
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-08-29 21:06:06 +0200

    addedsumQuads method sumQuads

--- TrurlBank.java
+++ TrurlBank.java
@@ -1,6 +1,15 @@
 package main;
 
 public class TrurlBank {
+    public int countSumOfDigits(int number) {
+        int result = 0;
+        while (number != 0) {
+            result += number % 10;
+            number /= 10;
+        }
+        return result;
+    }
+
     public int sumQuads(int n) {
         int result = 0;
         while (n > 0) {
@@ -15,5 +24,8 @@ public class TrurlBank {
 
         //Should be 14 - 1 + 4 + 9
         System.out.println(bank.sumQuads(3));
+
+        //Should be 16 - 5 + 4 + 7
+        System.out.println(bank.countSumOfDigits(547));
     }
 }
