d58f6ad521028af0237c69bb7d4282ba57c3b05d
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-08-30 22:00:52 +0200

    complited class

--- SpaceShip.java
+++ SpaceShip.java
@@ -1,5 +1,48 @@
 package main;
 
 public class SpaceShip {
-    public String name = "Voyager";
+    private String name;
+    private String serialNumber;
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        if (!serialNumber.startsWith("SN") || serialNumber.length() != 8) {
+            return;
+        }
+        this.serialNumber = serialNumber;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.isBlank() || name.length() > 100) {
+            return;
+        }
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Name is " + getName() + ", serial number is " + getSerialNumber());
+    }
+
+    public static void main(String[] args) {
+        SpaceShip ship = new SpaceShip();
+
+        ship.setSerialNumber("SN506788");
+        System.out.println(ship.getSerialNumber()); //Should be SN506788
+
+        ship.setSerialNumber("EE123456");
+        System.out.println(ship.getSerialNumber()); //Should be SN506788 - old value
+
+        ship.setName("Voyager");
+        ship.setSerialNumber("SN506788");
+
+        ship.printInfo();
+    }
 }
