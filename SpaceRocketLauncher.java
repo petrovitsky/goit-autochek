3f334605d7aa473f35db210a4327a9c63ae607b8
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-08-30 22:18:46 +0200

    added class with impl

---
+++ SpaceRocketLauncher.java
@@ -0,0 +1,61 @@
+package main;
+
+public class SpaceRocketLauncher {
+    private int bigRocketCount;
+    private int smallRocketCount;
+
+    public int getBigRocketCount() {
+        return bigRocketCount;
+    }
+
+    public void setBigRocketCount(int bigRocketCount) {
+        if(bigRocketCount < 0 || bigRocketCount > 100){
+            return;
+        }
+        this.bigRocketCount = bigRocketCount;
+    }
+
+    public int getSmallRocketCount() {
+        return smallRocketCount;
+    }
+
+    public void setSmallRocketCount(int smallRocketCount) {
+        if (smallRocketCount < 0 || smallRocketCount > 100) {
+            return;
+        }
+        this.smallRocketCount = smallRocketCount;
+    }
+    public void launchBigRocket(){
+        if (getBigRocketCount() > 0){
+            setBigRocketCount(getBigRocketCount() - 1);
+            System.out.println("Launch big rocket");
+        }
+    }
+    public void launchSmallRocket(){
+        if (getSmallRocketCount() > 0){
+            setSmallRocketCount(getSmallRocketCount() - 1);
+            System.out.println("Launch small rocket");
+        }
+    }
+    public int getTotalPower(){
+        return getBigRocketCount() * 100 + getSmallRocketCount() * 50;
+    }
+}
+
+class TestRocketLauncher {
+    public static void main(String[] args) {
+        SpaceRocketLauncher launcher = new SpaceRocketLauncher();
+        launcher.setBigRocketCount(5);
+        launcher.setSmallRocketCount(10);
+
+        System.out.println("Power is " + launcher.getTotalPower());
+
+        launcher.launchBigRocket();
+        System.out.println("Big rocket count is " + launcher.getBigRocketCount());
+
+        launcher.launchSmallRocket();
+        System.out.println("Small rocket count is " + launcher.getSmallRocketCount());
+
+        System.out.println("Power is " + launcher.getTotalPower());
+    }
+}
