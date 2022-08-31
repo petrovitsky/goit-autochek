5cd1a67f9bfea0c5c0c3082c97dec2fe99eb09da
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-08-31 21:41:08 +0200

    updeted class

--- module5/LaunchCalculator.java
+++ module5/LaunchCalculator.java
@@ -1,4 +1,26 @@
 package main.module5;
 
-public class LaunchCalculator {
+class LaunchCalculator {
+    public int calculateTotalPrice(Spaceport spaceport, int passengerCount) {
+        return spaceport.calculateTotalPrice(passengerCount);
+    }
+
+    public static void main(String[] args) {
+        LaunchCalculator calculator = new LaunchCalculator();
+
+        int jupiterStarPrice = calculator.calculateTotalPrice(new JupiterStar(), 100);
+        System.out.println("JupiterStar price = " + jupiterStarPrice); //700
+
+        int mercuryBeatPrice = calculator.calculateTotalPrice(new MercuryBeat(), 100);
+        System.out.println("MercuryBeat price = " + mercuryBeatPrice); //1500
+
+        int sunHeavenPrice = calculator.calculateTotalPrice(new SunHeaven(), 100);
+        System.out.println("SunHeaven price = " + sunHeavenPrice); //2000
+    }
 }
+
+abstract class Spaceport {
+    public abstract int calculateTotalPrice(int passengerCount);
+}
+
+
