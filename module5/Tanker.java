141f0fc30d33c842514e68d716d58951635d71ab
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-08-31 21:18:05 +0200

    added class Tanker

---
+++ module5/Tanker.java
@@ -0,0 +1,39 @@
+package main.module5;
+
+public class Tanker {
+    private String serialNumber = "";
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    protected String getFuelType(){
+        return "A500";
+    }
+
+    public void refuel(int amount){
+        System.out.println("Add " + amount + " of " + getFuelType() + " tanker serial number is " + serialNumber);
+    }
+}
+
+class XFuelTanker extends Tanker{
+    @Override
+    protected String getFuelType() {
+        return "XFuel";
+    }
+}
+
+class MX200Tanker extends Tanker{
+    @Override
+    protected String getFuelType() {
+        return "MX200";
+    }
+
+
+}
+
+
