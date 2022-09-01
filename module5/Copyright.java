26b04d866d5fe55072f82a92fab8a78b90f28245
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-01 20:41:38 +0200

    added class public final class Copyright {

---
+++ module5/Copyright.java
@@ -0,0 +1,12 @@
+package main.module5;
+
+final class Copyright {
+    public void print(){
+        System.out.println("SpaceWarriors Inc.");
+    }
+
+    public static void main(String[] args) {
+        new Copyright().print();
+    }
+}
+
