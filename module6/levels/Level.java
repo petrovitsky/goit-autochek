b8e6423d1f85031179aa6ff74514f893963e562a
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-15 22:53:48 +0200

    added implementations

--- module6/levels/Level.java
+++ module6/levels/Level.java
@@ -2,13 +2,34 @@ package main.module6.levels;
 
 public class Level {
     private String name;
+    Point[] points;
 
-    public Level(String name) {
+    Level(String name, Point[] points) {
         this.name = name;
+        this.points = points;
+
     }
 
     @Override
     public String toString() {
-        return "Quarke level, name is " + name;
+        return "Quarke level, name is " + name + ", point count is " + points.length;
+    }
+
+    static class Point{
+        private int x;
+        private int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
     }
 }
--- module6/levels/LevelTest.java
+++ module6/levels/LevelTest.java
@@ -2,7 +2,13 @@ package main.module6.levels;
 
 public class LevelTest {
     public static void main(String[] args) {
-        //Quarke level, name is Test
-        System.out.println(new Level("Test"));
+        Level.Point p1 = new Level.Point(3, 5);
+        Level.Point p2 = new Level.Point(10, 100);
+        Level.Point p3 = new Level.Point(50, 40);
+
+        Level.Point[] points = {p1, p2, p3};
+
+        //Quarke level, name is Test, point count is 3
+        System.out.println(new Level("Test", points));
     }
 }
