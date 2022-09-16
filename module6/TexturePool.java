8a71a2ba0fc3f9195e0b5769c39f836a7b788351
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-17 00:00:32 +0200

    added implementations

---
+++ module6/TexturePool.java
@@ -0,0 +1,24 @@
+package main.module6;
+
+public class TexturePool {
+    private static TexturePool instance = new TexturePool();
+
+    private TexturePool() {
+    }
+
+    public static TexturePool getInstance() {
+
+        return instance;
+    }
+
+    public String getTexture(String textureName) {
+        return "Get texture " + textureName;
+    }
+}
+
+class TexturePoolTest {
+    public static void main(String[] args) {
+        //Get texture MainHero
+        System.out.println(TexturePool.getInstance().getTexture("MainHero"));
+    }
+}
--- module6/levelloader/LevelLoader.java
+++ module6/levelloader/LevelLoader.java
@@ -16,3 +16,4 @@ public class LevelLoader {
         return "Loading level " + levelName + " ...";
     }
 }
+
