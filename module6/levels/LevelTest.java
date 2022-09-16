51cfffc85c6aef49ae704a19854cfd82ee3aff51
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-16 23:03:01 +0200

    added implementations

--- module6/levels/LevelTest.java
+++ module6/levels/LevelTest.java
@@ -1,7 +1,10 @@
 package main.module6.levels;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class LevelTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Level.Point p1 = new Level.Point(3, 5);
         Level.Point p2 = new Level.Point(10, 100);
         Level.Point p3 = new Level.Point(50, 40);
@@ -12,5 +15,26 @@ public class LevelTest {
 
         //Quarke level, name is Quarke Intro, difficulty is Easy, point count is 3
         System.out.println(new Level(info, points));
+
+        byte[] input = new byte[]{1,2,3,4,5};
+        InputStream stream = new InputStream() {
+            private int index = 0;
+
+            @Override
+            public int read() throws IOException {
+                if (index == input.length) {
+                    return -1;
+                }
+                return input[index++];
+            }
+        };
+
+        int data = stream.read();
+        while (data != -1){
+            System.out.println("data = " + data);
+            data = stream.read();
+        }
+
+
     }
 }
---
+++ module9/CustomIO.java
@@ -0,0 +1,32 @@
+package main.module9;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CustomIO {
+    public static void main(String[] args) throws IOException {
+
+        byte [] input = new byte[] {12,33,45,32,123,44,12};
+
+        InputStream is = new InputStream() {
+            private int index = 0;
+            @Override
+            public int read() throws IOException {
+                if (index == input.length){
+                    return  -1;
+                }
+                return input[index++];
+            }
+        };
+
+        int data = 0;
+        while (data != -1 ) {
+            System.out.println(data);
+            data = is.read();
+        }
+
+        System.out.println("input.length = " + input.length);
+
+
+    }
+}
