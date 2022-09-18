478f3c0e40f92c2a3ce4dbe7248ef98810e499a8
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-18 10:40:10 +0200

    added implementations

---
+++ module6/AIPlayer.java
@@ -0,0 +1,35 @@
+package main.module6;
+
+public class AIPlayer {
+    private int hp;
+
+    public void setHp(int hp) {
+        if ((hp < 0) || (hp > 100)) {
+            throw new IllegalArgumentException();
+        } else {
+            this.hp = hp;
+        }
+    }
+}
+
+class AIPlayerTest {
+    public static void main(String[] args) {
+        AIPlayer player = new AIPlayer();
+
+        //Ok value
+        try {
+            player.setHp(50);
+            System.out.println("Ok value");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid value");
+        }
+
+        //Invalid value
+        try {
+            player.setHp(-1);
+            System.out.println("Ok value");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid value");
+        }
+    }
+}
