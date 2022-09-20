1088da97f23c5cab4d056040b98aa4cf3998671e
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-20 23:13:49 +0200

    added implementations

--- module7/WordSplitter.java
+++ module7/WordSplitter.java
@@ -1,6 +1,8 @@
 package main.module7;
 
 
+import java.util.Arrays;
+
 public class WordSplitter {
     public String[] split(String phrase){
         return phrase.toLowerCase().split("[^A-Za-zА-Яа-я]+");
@@ -8,3 +10,13 @@ public class WordSplitter {
 }
 
 
+class WordSplitterTest {
+    public static void main(String[] args) {
+        WordSplitter wordSplitter = new WordSplitter();
+
+        String[] words = wordSplitter.split("Hello      world");
+
+        //[hello, world]
+        System.out.println(Arrays.toString(words));
+    }
+}
