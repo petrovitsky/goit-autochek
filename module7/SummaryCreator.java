b0b0cd7c282e7578902d241fb7c6c661f6084f83
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-23 19:50:59 +0200

    added implementations

--- module7/SummaryCreator.java
+++ module7/SummaryCreator.java
@@ -3,16 +3,31 @@ package main.module7;
 public class SummaryCreator {
     public String create(String text){
         if(text.length() > 15) {
-            if(text.charAt(16) == 32){
-                return text.substring(0,15) + "...";
+            if(text.charAt(15) != 32){
+                return text.substring(0,15) + ("...");
             } else {
                 return text.substring(0,15);
             }
         }
-
-
         return text;
     }
 }
 
+class SummaryCreatorTest {
+    public static void main(String[] args) {
+
+        SummaryCreator summaryCreator = new SummaryCreator();
+
+        //Mars
+        String shortPhrase = "Mars";
+        System.out.println(summaryCreator.create(shortPhrase));
+
+        //Java is very po...
+        String longPhrase = "Java is very popular language";
+        System.out.println(summaryCreator.create(longPhrase));
+        // yxnodalynuleynv
+        String phrase = "yxnodalynuleynv ymydibwo";
+        System.out.println(summaryCreator.create(phrase));
+    }
+}
 
