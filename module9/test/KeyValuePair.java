10b74d50b756da7d4e75aa9173874769e02e6a52
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-28 00:37:51 +0200

    added implementations

--- module9/test/KeyValuePair.java
+++ module9/test/KeyValuePair.java
@@ -1,4 +1,52 @@
 package main.module9.test;
 
-public class KeyValuePair {
+public class KeyValuePair<K,V> {
+    private K key;
+    private V value;
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "KeyValuePair{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
 }
+
+class KeyValueTest {
+    public static void main(String[] args) {
+        KeyValuePair<String, Integer> javaSalary = new KeyValuePair<>();
+        javaSalary.setKey("Java");
+        javaSalary.setValue(5000);
+        System.out.println(javaSalary);
+        System.out.println("----- ---- ------");
+        KeyValuePair<String, Integer> javaSalary1 = new KeyValuePair<String, Integer>();
+        KeyValuePair<Integer, Float> yearAvgTemperature = new KeyValuePair<Integer, Float>();
+
+        javaSalary1.setKey("Java");
+        javaSalary1.setValue(5000);
+
+        yearAvgTemperature.setKey(3200);
+        yearAvgTemperature.setValue(15f);
+        System.out.println(javaSalary1);
+        System.out.println(yearAvgTemperature);
+    }
+}
+
+
---
+++ module9/test/WildCardTest.java
@@ -0,0 +1,66 @@
+package main.module9.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WildCardTest {
+    public static void driveAllTransport(List<? extends Transport> t) {
+        for (Transport transport : t) {
+            transport.drive();
+        }
+    }
+
+    public <T,S,V> void fillListOfEntyties(S data) {
+        return new ArrayList<S>(10 , new V()) ;
+    }
+
+
+    public static void main(String[] args) {
+        List<Transport> transports = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            transports.add(new Transport());
+        }
+
+        for (Transport t : transports) {
+            System.out.println(t);
+        }
+
+    }
+}
+
+class FirstCar {
+
+}
+
+class Transport extends FirstCar {
+    protected static int entityCounter;
+    private int index;
+
+    public int getIndex() {
+        return index;
+    }
+
+
+    public Transport() {
+        this.index = entityCounter;
+        entityCounter++;
+    }
+
+    public void drive() {
+        System.out.println("Transport is going");
+    }
+
+    @Override
+    public String toString() {
+        return "Transport{" +
+                "index=" + index +
+                '}';
+    }
+}
+
+class Car extends Transport {
+    @Override
+    public void drive() {
+        System.out.println("Car is deiving");
+    }
+}
