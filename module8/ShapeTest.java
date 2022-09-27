5c00966d53f1331957dc3782d9ffe6e4e8ab56a9
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-27 20:24:18 +0200

    added implementations

--- module8/ShapeTest.java
+++ module8/ShapeTest.java
@@ -5,6 +5,10 @@ package main.module8;
 public class ShapeTest {
 
     public static void main(String[] args) {
-
+        Shape [] shapes = { new Circle(), new Octagon(), new Quad(), new Rhombus(), new Triangle()};
+        ShapeInfo shapeInfo = new ShapePrinter();
+        for (Shape s: shapes) {
+            shapeInfo.print(s);
+        }
     }
 }
