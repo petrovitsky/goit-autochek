92e1a0fa21f8b04ea8cc7356061b1174336c3d89
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-29 21:49:54 +0200

    added implementations

---
+++ module11/ModuleTest.java
@@ -0,0 +1,15 @@
+package main.module11;
+
+public class ModuleTest {
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+
+@FunctionalInterface
+interface MathExamples{
+    int add(int a, int b);
+}
