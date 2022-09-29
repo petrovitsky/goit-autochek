968e16b30785ef89dd05b6865f2120bfb7fc8211
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-29 23:23:01 +0200

    added implementations

--- module9/homework/MyArrayList.java
+++ module9/homework/MyArrayList.java
@@ -1,6 +1,27 @@
 package main.module9.homework;
 
+import jdk.internal.util.ArraysSupport;
+
+import java.util.Arrays;
+
 public class MyArrayList<E> implements MyList<E> {
+
+    private static final int DEFAULT_CAPACITY = 16;
+    private int size;
+    private Object[] data;
+
+    private Object[] grow(int minCapacity) {
+        int oldCapacity = data.length;
+        if (oldCapacity > 0) {
+            int newCapacity = ArraysSupport.newLength(oldCapacity,
+                    minCapacity - oldCapacity, /* minimum growth */
+                    oldCapacity >> 1           /* preferred growth */);
+            return data = Arrays.copyOf(data, oldCapacity * );
+        } else {
+            return data = new Object[Math.max(DEFAULT_CAPACITY, minCapacity)];
+        }
+    }
+
     @Override
     public boolean add(E value) {
         return false;
