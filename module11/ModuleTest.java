dcf5627aa90b5887f218ffa3807e85b6e42d959a
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-09-29 22:02:03 +0200

    added implementations

--- module11/ModuleTest.java
+++ module11/ModuleTest.java
@@ -2,12 +2,15 @@ package main.module11;
 
 public class ModuleTest {
     public static void main(String[] args) {
-
+        int result = test(3, 4, (a, b) -> a * b);
+        System.out.println(result);
     }
 
+    public static int test (int a, int b, MathExamples m ){
+
+        return m.add(a,b);
+    }
 }
-
-
 
 @FunctionalInterface
 interface MathExamples{
