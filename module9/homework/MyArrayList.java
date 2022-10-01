7086c37bee9911bee36310b2dfb6ecbf8cfde607
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-10-01 22:11:33 +0200

    added implementations

--- module9/homework/MyArrayList.java
+++ module9/homework/MyArrayList.java
@@ -7,32 +7,36 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class MyArrayList<E> implements MyList<E> {
-
-    private static final int DEFAULT_CAPACITY = 10;
-
+    private static final int DEFAULT_CAPACITY = 1;
     private int size;
     private Object[] data;
-
     public MyArrayList (){
         data = (E[]) new Object[DEFAULT_CAPACITY];
+    }
+    public MyArrayList (int capacity){
+        data = (E[]) new Object[capacity];
     }
 
 
     private Object[] grow(int minCapacity) {
         int oldCapacity = data.length;
         if (oldCapacity > 0) {
-            int newCapacity = ArraysSupport.newLength(oldCapacity,
-                    minCapacity - oldCapacity, /* minimum growth */
-                    oldCapacity >> 1           /* preferred growth */);
-            return data = Arrays.copyOf(data, oldCapacity *  2);
+            return data = Arrays.copyOf(data, (int) (oldCapacity * 1.2));
         } else {
             return data = new Object[Math.max(DEFAULT_CAPACITY, minCapacity)];
         }
     }
+    private void add (E element, Object [] data, int s){
+           if ( s == data.length ) {
+               data = grow(s + 1);
+           }
+           data[s] = element;
+           size = s + 1;
 
+    }
     @Override
     public boolean add(E value) {
-      data[size++] = value;
+      add( value , data , size);
       return true;
     }
 
@@ -60,5 +64,9 @@ public class MyArrayList<E> implements MyList<E> {
     @Override
     public Iterator<E> iterator() {
         return null;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
