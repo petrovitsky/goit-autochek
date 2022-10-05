a5017d35d60ea061fdc6807b63f6d94462fd262f
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-10-05 23:11:45 +0200

    added implementations

---
+++ module9/test/GenericTest.java
@@ -0,0 +1,40 @@
+package main.module9.test;
+
+public class GenericTest {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static  <K ,V> void   summ ( Object<> ) {
+
+
+        Object obj = new Object();
+        Object ob = new Object(){};
+    }
+}
+class A extends Number{
+    @Override
+    public int intValue() {
+        return 0;
+    }
+
+    @Override
+    public long longValue() {
+        return 0;
+    }
+
+    @Override
+    public float floatValue() {
+        return 0;
+    }
+
+    @Override
+    public double doubleValue() {
+        return 0;
+    }
+
+    public static void foo (){
+        System.out.println("\"Foo method\" = " + "Foo method");
+    }
+}
