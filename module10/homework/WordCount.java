cab9b82021fc4b89f8469d99f523ea16a656ff21
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2022-10-20 00:08:56 +0200

    added implementations

---
+++ module10/homework/WordCount.java
@@ -0,0 +1,36 @@
+package main.module10.homework;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class WordCount {
+    public static void main(String[] args) {
+        frequencyCount("./words.txt");
+    }
+
+    public static void frequencyCount(String path) {
+        Map<String, Integer> resultMap = new HashMap<>();
+        List<Map.Entry<String, Integer>> list = new LinkedList<>();
+        Collections.sort(list, ((o1, o2) -> o2.getValue() - o1.getValue()));
+
+        try (Scanner scanner = new Scanner(new FileInputStream(path))) {
+            while (scanner.hasNext()) {
+                String s = scanner.next();
+                if (resultMap.containsKey(s)) {
+                    resultMap.put(s, resultMap.get(s) + 1);
+                } else {
+                    resultMap.put(s, 1);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.getMessage();
+        }
+
+        list.addAll(resultMap.entrySet());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getKey() + "\s" + list.get(i).getValue());
+        }
+    }
+}
